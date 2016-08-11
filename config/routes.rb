4e96ff92ff1fb925db1b577798934f819f723f0c
uma parakala <umaparakala@gmail.com>
2016-08-11 05:24:25 +0000

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
+  get 'users/new'
+
   get  '/help',    to: 'static_pages#help'
   get  '/about',   to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
   
   root 'static_pages#home'
+  
+  get  '/signup',  to: 'users#new'
+
   
 end
