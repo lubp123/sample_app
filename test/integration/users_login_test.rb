79fa1990bf6204cd1574b647bdf5643fa48d8cf6
uma parakala <umaparakala@gmail.com>
2016-08-24 00:33:03 +0000

    finished basic + advanced login

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -20,7 +20,7 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
      
   end 
   
-  test "login with valid information" do
+  test "login with valid information followed by logout" do
     get login_path
     post login_path, params: { session: { email:    @user.email,
                                           password: 'password' } }
@@ -35,6 +35,7 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     delete logout_path
     assert_not is_logged_in?
     assert_redirected_to root_url
+    delete logout_path
     follow_redirect!
     assert_select "a[href=?]", login_path
     assert_select "a[href=?]", logout_path,      count: 0
